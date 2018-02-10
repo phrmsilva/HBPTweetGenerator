63a77383f1fd7cf81ba3d0254095fe983bcaa8d9
phrmsilva <phrmsilva@gmail.com>
2018-02-10 14:04:53 -0500

    Add to file functionality

--- twitter/get_tweets.py
+++ twitter/get_tweets.py
@@ -9,11 +9,11 @@ api = twitter.Api(consumer_key=os.environ['CONSUMER_KEY'],
 DEFAULT_COUNT = 200
 
 def get_tweets(user_name, count=DEFAULT_COUNT):
-    tweets_text = [s.text.encode('ascii', 'ignore') for s in api.GetUserTimeline(include_rts=False, 
+    tweets_text = [s.text.encode('ascii', 'ignore') for s in api.GetUserTimeline(include_rts=False,
                                                                                  screen_name=user_name)]
-    
+
     if (count <= DEFAULT_COUNT):
-        tweets_text = [s.text for s in api.GetUserTimeline(include_rts=True, screen_name=user_name)]
+        tweets_text = [s.text for s in api.GetUserTimeline(include_rts=False, screen_name=user_name)]
     else:
         tweets = api.GetUserTimeline(include_rts=True, screen_name=user_name)
         remaining = count - DEFAULT_COUNT
@@ -21,7 +21,7 @@ def get_tweets(user_name, count=DEFAULT_COUNT):
         tweets_text = [t.text for t in tweets]
 
         while remaining > 0:
-            to_request = 0 
+            to_request = 0
             if remaining > 200:
                 to_request = 200
                 remaining -= 200
@@ -29,7 +29,7 @@ def get_tweets(user_name, count=DEFAULT_COUNT):
                 to_request = remaining
                 remaining = 0
 
-        tweets = api.GetUserTimeline(include_rts=True, screen_name=user_name, count=to_request, max_id=last_tweet)
+        tweets = api.GetUserTimeline(include_rts=False, screen_name=user_name, count=to_request, max_id=last_tweet)
         if len(tweets) > 0:
             last_tweet = tweets[-1].id
         tweets_text += [t.text for t in tweets]
@@ -38,3 +38,10 @@ def get_tweets(user_name, count=DEFAULT_COUNT):
 def unicode_to_str(uc_list):
     return [s.encode('ascii', 'ignore') for s in uc_list]
 
+def get_tweets_to_file(user_name, count=DEFAULT_COUNT, file_path=None):
+    if file_path is None:
+        file_path = './resources/' + user_name + '.txt'
+    f = open(file_path, 'w')
+    for t in get_tweets(user_name, count):
+        f.write(t + '\n')
+    f.close()
